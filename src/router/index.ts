792f544fb0b07ccab4d7bd53ddcc77f6fcfca949
Herbert Nordson <herbert_nordson@hotmail.com>
2025-03-20 03:40:09 -0300

    feat: create login page

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import SignIn from "@/views/SignInView.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: SignIn,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
